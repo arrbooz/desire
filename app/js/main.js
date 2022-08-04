7911e99339153cd6769c9c1ece82dd712bd66943
arrbooz <arrbooz@gmail.com>
2022-08-04 21:11:10 +0300

    adaptive done

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,11 @@ $(function  () {
         $('.menu').toggleClass('menu--open');
     });
 
+
+    if($(window).width() < 651) {
+        $('.works-path__item--measuring').appendTo($('.works-path__item-box'));
+    }
+
    
 
     $('.top__slider').slick({
@@ -26,6 +31,51 @@ $(function  () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1750,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1551,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 420,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            
+        ]
     });
 
     $('.article-slider__box').slick({
